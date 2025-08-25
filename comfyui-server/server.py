3dc866c111abf4cedbabd2e7e60d2627075da896
Shellishack <40737228+Shellishack@users.noreply.github.com>
2025-08-25 11:56:56 +0800

    Refactor project

--- comfyui-server/server.py
+++
@@ -1,3 +0,0 @@
-# This needs to handle API requests and then build a custom RunPod serverless image for ComfyUI.
-# The image should contain the necessary model files (or maybe can be done via a volume mount?)
-# - export the model files and custom_nodes files to the volume mount or build an image with these (via comfy cli?)
--- src/components/service/service.tsx
+++ src/components/service/service.tsx
@@ -132,7 +132,7 @@ export default function Service({
                 setIsDeployModelsOpen(true);
               }}
             >
-              Deploy Models
+              Request Models
             </Button>
 
             {workflow && (
